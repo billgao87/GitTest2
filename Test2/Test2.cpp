3e7a7acf104dbc260759c7638bdbe26119fd93c7
billgao <qingtongjian1987@163.com>
2020-07-15 10:23:42 +0800

    1. add gittest2

---
+++ Test2/Test2.cpp
@@ -0,0 +1,22 @@
+// Test2.cpp : 定义 DLL 应用程序的导出函数。
+//
+
+#include "stdafx.h"
+#include "Test2.h"
+
+
+// 这是导出变量的一个示例
+TEST2_API int nTest2=0;
+
+// 这是导出函数的一个示例。
+TEST2_API int fnTest2(void)
+{
+    return 42;
+}
+
+// 这是已导出类的构造函数。
+// 有关类定义的信息，请参阅 Test2.h
+CTest2::CTest2()
+{
+    return;
+}
